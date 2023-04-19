0166b94c0b98231fe2cc00b99a1d5988dad7cb9e
Christopher Sieh <c.sieh@steado.de>
2023-04-19 16:48:25 +0200

    rospcl changed

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,13 @@
-fn filter(input: rospcl::Pointcloud3F32) -> rospcl::Pointcloud3F32 {
+use rospcl::Pointcloud3F32;
+use rosrust_msg::sensor_msgs::PointCloud2;
+
+fn filter(input: Pointcloud3F32) -> Pointcloud3F32 {
     let mut output = Vec::new();
     for point in input {
         output.push(point); // TODO: implement filtering arguments here
     }
 
-    rospcl::Pointcloud3F32::new(output)
+    Pointcloud3F32::new(output)
 }
 
 fn main() {
@@ -20,15 +23,15 @@ fn main() {
     };
 
     // subscribe to a topic
-    let subber = rosrust::subscribe("/velodyne_points", 100, move |msg: rosrust_msg::sensor_msgs::PointCloud2| {
+    let subber = rosrust::subscribe("/velodyne_points", 100, move |msg: PointCloud2| {
         let incoming_header = msg.header.clone();
-        match rospcl::Pointcloud3F32::from_msg(msg) {
+        match Pointcloud3F32::try_from(msg) {
             Err(e) => {
                 rosrust::ros_err!("Error: {:?}", e);
             }
             Ok(cloud) => {
                 let filtered_cloud = filter(cloud);
-                let mut out_msg = filtered_cloud.to_msg().unwrap();
+                let mut out_msg: PointCloud2 = filtered_cloud.try_into().unwrap();
                 out_msg.header = incoming_header;
 
                 if let Err(e) = pubber.send(out_msg) {
