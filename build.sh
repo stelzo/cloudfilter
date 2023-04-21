daa13d8d4ec667ea02d0401bc5af98953d031249
Christopher Sieh <c.sieh@steado.de>
2023-04-21 11:04:41 +0200

    aabb

---
+++ build.sh
@@ -0,0 +1,105 @@
+#!/bin/bash
+set -e
+
+### A script to build rust binaries and copy them to a catkin package bin folder.
+### The script is intended to be used in a catkin package with a Cargo.toml file. It installs rust if it is not installed.
+### Usage: ./build.sh DEBUG|RELEASE [binary1] [binary2] ...
+
+# check first parameter case insensitive(!) debug or release
+if [[ ! "$1" =~ ^(DEBUG|RELEASE|debug|release|Debug|Release)$ ]]; then
+  echo "Usage: ./build.sh DEBUG|RELEASE [binary1] [binary2] ..."
+  exit
+fi
+
+# set binaries except first argument
+binaries=("${@:2}")
+
+# check if given argument for DEBUG or RELEASE build
+if [[ "$1" =~ ^(DEBUG|debug|Debug)$ ]]; then
+  CARGO_BUILD_TYPE="debug"
+else
+  CARGO_BUILD_TYPE="release"
+fi
+
+# current dir name as package name
+BASE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+DIR="$(basename "$BASE_DIR")"
+
+# set dir name as default if no binaries are given
+if [ "${#binaries[@]}" -eq 0 ]; then
+  binaries=("$DIR")
+fi
+
+catkin_pkg_bin_path="$(eval "catkin locate -d")/lib/$DIR"
+
+mkdir -p "$catkin_pkg_bin_path"
+
+# install rust using curl or wget. If both are not installed, curl is installed but needs sudo.
+install_rust() {
+  if ! command -v curl &> /dev/null
+  then
+    if ! command -v wget &> /dev/null
+      then
+        echo "curl and wget could not be found, please install curl or wget and try again."
+        echo "sudo apt install curl"
+        exit 1
+      else
+        wget https://sh.rustup.rs -O - | sh -s -- -y
+      fi
+  else
+    curl https://sh.rustup.rs -sSf | sh -s -- -y
+  fi
+
+  source "$HOME"/.cargo/env
+}
+
+# check if rust is installed
+if ! command -v rustc &> /dev/null
+then
+  echo "rustc could not be found, installing rust..."
+  install_rust
+
+  # check if rust is installed again
+  if ! command -v rustc &> /dev/null
+  then
+    echo "rustc could not be found"
+    exit
+  fi
+fi
+
+# check if cargo is installed
+if ! command -v cargo &> /dev/null
+then
+  echo "cargo could not be found"
+  install_rust
+
+  # check if cargo is installed again
+  if ! command -v cargo &> /dev/null
+  then
+    echo "cargo could not be found"
+    exit
+  fi
+fi
+
+### BUG WORKAROUND
+# For generation, a few messages have problems so we reset to all default messages. Delete this part if you need messages from your workspace.
+unset CMAKE_PREFIX_PATH
+unset ROS_PACKAGE_PATH
+export CMAKE_PREFIX_PATH=/opt/ros/$ROS_DISTRO
+export ROS_PACKAGE_PATH=/opt/ros/$ROS_DISTRO/share
+### END BUG WORKAROUND
+
+cd "$BASE_DIR"
+
+# if debug, omit the release flag
+if [ "$CARGO_BUILD_TYPE" = "debug" ]; then
+  cargo build --bins
+else
+  cargo build --release --bins
+fi
+
+# copy binaries to catkin package bin folder
+for binary in "${binaries[@]}"
+do
+  cp "target/$CARGO_BUILD_TYPE/$binary" "$catkin_pkg_bin_path"
+done
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,24 @@
+use nalgebra::RealField;
 use rospcl::Pointcloud3F32;
 use rosrust_msg::sensor_msgs::PointCloud2;
 
-fn filter(input: Pointcloud3F32) -> Pointcloud3F32 {
+struct AABB {
+    min: [f32; 3],
+    max: [f32; 3],
+}
+
+fn aabb_filter(input: Pointcloud3F32, aabb: AABB) -> Pointcloud3F32 {
     let mut output = Vec::new();
     for point in input {
-        output.push(point); // TODO: implement filtering arguments here
+        if point[0] >= aabb.min[0]
+            && point[0] <= aabb.max[0]
+            && point[1] >= aabb.min[1]
+            && point[1] <= aabb.max[1]
+            && point[2] >= aabb.min[2]
+            && point[2] <= aabb.max[2]
+        {
+            output.push(point);
+        }
     }
 
     Pointcloud3F32::new(output)
@@ -15,27 +29,43 @@ fn main() {
 
     // advertise a topic
     let pubber = match rosrust::publish("filtered", 100) {
-        Ok(p) => {
-            rosrust::ros_info!("Publishing to filtered topic");
-            p
-        },
+        Ok(p) => p,
         Err(_) => panic!("Failed to publish to filtered topic"),
     };
 
+    let filter_type: String = rosrust::param("~filter_type").unwrap().get().unwrap_or("aabb".to_string());
+    let aabb_min_x = rosrust::param("~aabb_min_x").unwrap().get().unwrap_or(f32::MIN) as f32;
+    let aabb_max_x = rosrust::param("~aabb_max_x").unwrap().get().unwrap_or(f32::MAX) as f32;
+    let aabb_min_y = rosrust::param("~aabb_min_y").unwrap().get().unwrap_or(f32::MIN) as f32;
+    let aabb_max_y = rosrust::param("~aabb_max_y").unwrap().get().unwrap_or(f32::MAX) as f32;
+    let aabb_min_z = rosrust::param("~aabb_min_z").unwrap().get().unwrap_or(f32::MIN) as f32;
+    let aabb_max_z = rosrust::param("~aabb_max_z").unwrap().get().unwrap_or(f32::MAX) as f32;
+
     // subscribe to a topic
-    let subber = rosrust::subscribe("/velodyne_points", 100, move |msg: PointCloud2| {
+    let subber = rosrust::subscribe("cloud", 100, move |msg: PointCloud2| {
         let incoming_header = msg.header.clone();
         match Pointcloud3F32::try_from(msg) {
             Err(e) => {
                 rosrust::ros_err!("Error: {:?}", e);
             }
             Ok(cloud) => {
-                let filtered_cloud = filter(cloud);
-                let mut out_msg: PointCloud2 = filtered_cloud.try_into().unwrap();
-                out_msg.header = incoming_header;
+                match filter_type.as_str() {
+                    "aabb"=> {
+                        let aabb = AABB {
+                            min: [aabb_min_x as f32, aabb_min_y as f32, aabb_min_z as f32],
+                            max: [aabb_max_x as f32, aabb_max_y as f32, aabb_max_z as f32],
+                        };
+                        let filtered_cloud = aabb_filter(cloud, aabb);
+                        let mut out_msg: PointCloud2 = filtered_cloud.try_into().unwrap();
+                        out_msg.header = incoming_header;
 
-                if let Err(e) = pubber.send(out_msg) {
-                    rosrust::ros_err!("Could not send filtered pointcloud: {:?}", e);
+                        if let Err(e) = pubber.send(out_msg) {
+                            rosrust::ros_err!("Could not send filtered pointcloud: {:?}", e);
+                        }
+                    }
+                    _ => {
+                        rosrust::ros_err!("Unknown filter type: {}", filter_type);
+                    }
                 }
             }
         }
@@ -45,6 +75,5 @@ fn main() {
         rosrust::ros_err!("Error: {:?}", e);
     }
 
-    rosrust::ros_info!("Spinning...");
     rosrust::spin();
 }
