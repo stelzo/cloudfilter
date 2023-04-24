38d4965f285ef8e7f0859b6290d7d44f02649874
Christopher Sieh <c.sieh@steado.de>
2023-04-24 13:12:51 +0200

    updated conv api

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,4 @@
-use std::iter::Map;
-use rospcl::Pointcloud3F32;
+use ros_pointcloud2::ConvertXYZI;
 use rosrust_msg::sensor_msgs::PointCloud2;
 use tf_rosrust::{TfListener};
 
@@ -8,8 +7,10 @@ struct AABB {
     max: [f32; 3],
 }
 
+type Point = ros_pointcloud2::pcl_utils::PointXYZI;
+
 #[inline]
-fn aabb_filter(input: Pointcloud3F32, aabb: AABB, tf: &tf_rosrust::transforms::geometry_msgs::Transform) -> Pointcloud3F32 {
+fn aabb_filter(input: ConvertXYZI, aabb: AABB, tf: &tf_rosrust::transforms::geometry_msgs::Transform) -> Vec<Point> {
     // transformation from the pointcloud frame to the global zero frame
     let tf = nalgebra::geometry::Isometry3::from_parts(
         nalgebra::geometry::Translation3::new(tf.translation.x as f32, tf.translation.y as f32, tf.translation.z as f32),
@@ -21,27 +22,30 @@ fn aabb_filter(input: Pointcloud3F32, aabb: AABB, tf: &tf_rosrust::transforms::g
         )),
     );
 
-    Pointcloud3F32::new(input.map(|point| {
-        let point = nalgebra::Point3::new(point[0], point[1], point[2]);
-        tf.transform_point(&point) // transform
+    input.map(|point| {
+        (tf.transform_point(&nalgebra::Point3::new(point.x, point.y, point.z)), point.intensity) // transform
     }).filter(|point| { // aabb filter
-        point[0] >= aabb.min[0]
-            && point[0] <= aabb.max[0]
-            && point[1] >= aabb.min[1]
-            && point[1] <= aabb.max[1]
-            && point[2] >= aabb.min[2]
-            && point[2] <= aabb.max[2]
+        point.0[0] >= aabb.min[0]
+            && point.0[0] <= aabb.max[0]
+            && point.0[1] >= aabb.min[1]
+            && point.0[1] <= aabb.max[1]
+            && point.0[2] >= aabb.min[2]
+            && point.0[2] <= aabb.max[2]
     }).map(|point| {
-        [point.x, point.y, point.z] // convert back to array
-    }).collect())
+        Point {
+            x: point.0[0],
+            y: point.0[1],
+            z: point.0[2],
+            intensity: point.1,
+        }
+    }).collect()
 }
 
 fn main() {
     rosrust::init("cloudfilter");
     let listener = TfListener::new();
 
-    // advertise a topic
-    let pubber = match rosrust::publish("filtered", 100) {
+    let pubber = match rosrust::publish("cloud", 100) {
         Ok(p) => p,
         Err(_) => panic!("Failed to publish to filtered topic"),
     };
@@ -57,17 +61,19 @@ fn main() {
     let global_zero_frame: String = rosrust::param("~global_zero_frame").unwrap().get().unwrap_or("base_footprint".to_string());
 
     // subscribe to a topic
-    let subber = rosrust::subscribe("cloud", 100, move |msg: PointCloud2| {
+    let subber = rosrust::subscribe("/velodyne_points", 100, move |msg: PointCloud2| {
         let tf = listener.lookup_transform(global_zero_frame.as_str(), msg.header.frame_id.as_str(), msg.header.stamp);
         let tf = match tf {
             Ok(t) => t,
             Err(e) => {
-                rosrust::ros_err!("TF: {:?}", e);
+                rosrust::ros_warn!("TF: {:?}", e);
                 return;
             }
         };
         let incoming_header = msg.header.clone();
-        match Pointcloud3F32::try_from(msg) {
+
+
+        match ros_pointcloud2::ConvertXYZI::try_from(msg) {
             Err(e) => {
                 rosrust::ros_err!("Error: {:?}", e);
             }
@@ -79,12 +85,19 @@ fn main() {
                             max: [aabb_max_x as f32, aabb_max_y as f32, aabb_max_z as f32],
                         };
                         let filtered_cloud = aabb_filter(cloud, aabb, &tf.transform);
-                        let mut out_msg: PointCloud2 = filtered_cloud.try_into().unwrap();
-                        out_msg.header = incoming_header;
-                        out_msg.header.frame_id = global_zero_frame.clone();
+                        let filtered_msg: Result<PointCloud2, ros_pointcloud2::ConversionError> = ros_pointcloud2::ConvertXYZI::try_from(filtered_cloud).unwrap().try_into();
+                        match filtered_msg {
+                            Ok(mut out_msg) => {
+                                out_msg.header = incoming_header;
+                                out_msg.header.frame_id = global_zero_frame.clone();
 
-                        if let Err(e) = pubber.send(out_msg) {
-                            rosrust::ros_err!("Could not send filtered pointcloud: {:?}", e);
+                                if let Err(e) = pubber.send(out_msg) {
+                                    rosrust::ros_err!("Could not send filtered pointcloud: {:?}", e);
+                                }
+                            }
+                            Err(e) => {
+                                rosrust::ros_err!("Could not convert filtered pointcloud: {:?}", e);
+                            }
                         }
                     }
                     _ => {
